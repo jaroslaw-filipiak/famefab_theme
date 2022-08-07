d80f6f6770cef1ed3afbc24a6341efff31680fde
jaroslaw-filipiak <info@j-filipiak.pl>
2022-08-07 12:26:53 +0200

    detail 2

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,10 @@ export default defineConfig({
         homepage: resolve(__dirname, './front-pages/home/index.html'), // homepage
         manage: resolve(__dirname, './front-pages/manage/index.html'),
         make: resolve(__dirname, './front-pages/make/index.html'),
+        overOns: resolve(__dirname, './front-pages/over-ons/index.html'),
         contact: resolve(__dirname, './front-pages/contact-us/index.html'),
+        detail: resolve(__dirname, './front-pages/detail/index.html'),
+        caseDetail: resolve(__dirname, './front-pages/case-detail/index.html'),
       },
       output: {
         entryFileNames: `[name].js`,
