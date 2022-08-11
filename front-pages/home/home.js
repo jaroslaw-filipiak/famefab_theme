c9bdafe357d6aa05fedd0e893c65a3839f54ec9b
jaroslaw-filipiak <info@j-filipiak.pl>
2022-08-11 10:28:54 +0200

    test gsap

--- front-pages/home/home.js
+++ front-pages/home/home.js
@@ -1,1 +1,42 @@
-console.log('only homepage..');
+import gsap from 'gsap';
+
+// big logo reveal on load
+gsap.from('.logo_big', {
+  scrollTrigger: {
+    trigger: '.hello',
+    scroller: '.smooth-scroll',
+    // scrub: true,
+    markers: true,
+  },
+  yPercent: 200,
+  duration: 1,
+  ease: 'easeIn',
+});
+
+// gsap.from('.gsap__reveal', {
+//   scrollTrigger: {
+//     trigger: '.hello',
+//     scroller: '.smooth-scroll',
+//     // scrub: true,
+//     markers: true,
+//   },
+//   yPercent: 20,
+//   opacity: 0,
+//   duration: 1,
+//   ease: 'easeIn',
+// });
+
+gsap.from('.gsap__reveal-heading', {
+  scrollTrigger: {
+    trigger: '.who-we-are',
+    scroller: '.smooth-scroll',
+    start: 'top center',
+    // scrub: true,
+    markers: true,
+  },
+  yPercent: 60,
+  xPercent: 2,
+  opacity: 0,
+  duration: 1,
+  ease: 'easeIn',
+});
--- main.js
+++ main.js
@@ -1,4 +1,60 @@
 import './main.scss';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import LocomotiveScroll from 'locomotive-scroll';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('.smooth-scroll'),
+  smooth: true,
+});
+
+scroll.on('scroll', ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy('.smooth-scroll', {
+  scrollTop(value) {
+    return arguments.length
+      ? scroll.scrollTo(value, 0, 0)
+      : scroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector('.smooth-scroll').style.transform
+    ? 'transform'
+    : 'fixed',
+});
+
+const $bigBall = document.querySelector('.cursor__ball--big');
+const $smallBall = document.querySelector('.cursor__ball--small');
+const $hoverables = document.querySelectorAll('.hoverable');
+
+// Listeners
+document.body.addEventListener('mousemove', onMouseMove);
+for (let i = 0; i < $hoverables.length; i++) {
+  $hoverables[i].addEventListener('mouseenter', onMouseHover);
+  $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+}
+
+// Move the cursor
+function onMouseMove(e) {
+  gsap.to($bigBall, 0.4, {
+    x: e.pageX - 15,
+    y: e.pageY - 15,
+  });
+  gsap.to($smallBall, 0.1, {
+    x: e.pageX - 5,
+    y: e.pageY - 7,
+  });
+}
 
 import {
   handleScreenMenuLinksOnHover,
@@ -17,4 +73,40 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   init();
+});
+
+ScrollTrigger.defaults({
+  markers: true,
+});
+
+// let tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: '.logo_big',
+//     scroller: '.smooth-scroll',
+//     scrub: true,
+//     markers: true,
+//     start: '20px 95%',
+//     end: 'bottom 40px',
+//   },
+// });
+
+// ScrollTrigger.create({
+//   animation: tl,
+//   trigger: '.hello__logo-trigger',
+// });
+
+// scrollTrigger: {
+//   trigger: '.logo_big',
+//   scroller: '.smooth-scroll',
+//   scrub: true,
+//   markers: true,
+//   start: '20px 95%',
+//   end: 'bottom 40px',
+// },
+
+const test = document.querySelector('#who-we-are');
+const button = document.querySelector('.link-to-who-we-are');
+
+button.addEventListener('click', () => {
+  scroll.scrollTo(test);
 });
