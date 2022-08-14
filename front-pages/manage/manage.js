74cd61116a33b4459f6e2f079b1f839fe608b702
jaroslaw-filipiak <info@j-filipiak.pl>
2022-08-14 10:26:43 +0200

    manage page gsap

--- front-pages/manage/manage.js
+++ front-pages/manage/manage.js
@@ -1,3 +1,60 @@
+import gsap from 'gsap';
+
+// blank-hero--content color-green
+// make title
+gsap.from('.blank-hero--content', {
+  scrollTrigger: {
+    trigger: '.blank-hero',
+    scroller: '.smooth-scroll',
+    // scrub: true,
+    markers: false,
+  },
+
+  yPercent: -150,
+  duration: 1,
+  ease: 'easeIn',
+});
+
+// make title
+gsap.from('.blank-hero h1', {
+  scrollTrigger: {
+    trigger: '.blank-hero',
+    scroller: '.smooth-scroll',
+    // scrub: true,
+    markers: false,
+  },
+  yPercent: 200,
+  duration: 1,
+  ease: 'easeIn',
+});
+
+// make title
+gsap.to('.blank-hero h1', {
+  scrollTrigger: {
+    trigger: '.blank-hero-title-trigger',
+    scroller: '.smooth-scroll',
+    scrub: true,
+    markers: false,
+  },
+  xPercent: 200,
+  duration: 1,
+  ease: 'easeIn',
+});
+
+// bg scale
+gsap.to('.blank-hero-title-trigger', {
+  //   opacity: 0,
+  backgroundSize: '100%',
+  //   scale: 2.5,
+  scrollTrigger: {
+    trigger: '.blank-hero-title-trigger',
+    scroller: '.smooth-scroll',
+    scrub: true,
+    start: 'top bottom',
+    // end: 'top center',
+  },
+});
+
 export default function handleInfluencersOnHover() {
   const influencersWrapper = document.querySelector('.influencers');
   const influencersList = document.querySelectorAll('.influencers--list-item');
