c80c1a4e7786d1c2a3588b0676e6e78d61d0dd33
jaroslaw-filipiak <info@j-filipiak.pl>
2022-09-07 17:05:01 +0200

    default cursor

--- template-parts/footer-dynamic-data.php
+++ template-parts/footer-dynamic-data.php
@@ -166,25 +166,3 @@
 <div class="d-none d-xl-flex single-case--title color-green">
     Cases</div>
 <?php } ?>
-
-
-
-<?php if(!wp_is_mobile()) { ?>
-
-<!-- cursor -->
-<div class="cursor">
-    <div class="cursor__ball cursor__ball--big ">
-        <svg height="30" width="30">
-            <circle cx="15" cy="15" r="12" stroke-width="0"></circle>
-        </svg>
-    </div>
-
-    <div class="cursor__ball cursor__ball--small">
-        <svg height="10" width="10">
-            <circle cx="5" cy="5" r="4" stroke-width="0"></circle>
-        </svg>
-    </div>
-</div>
-<!--  <-- cursor end -->
-
-<?php } ?>
