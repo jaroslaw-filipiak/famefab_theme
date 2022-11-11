3815395df0d168263ea8ff2805417ac4ebde8952
jaroslaw-filipiak <info@j-filipiak.pl>
2022-11-11 18:31:39 +0100

    font test

--- header.php
+++ header.php
@@ -24,6 +24,113 @@
 	<link rel="preload" href="<?php echo get_theme_file_uri() ?>/public/fonts/helvetica/HelveticaLTW01-Roman.woff2"
 		as="font" type="font/woff2" crossorigin>
 
+	<style>
+	@font-face {
+		font-family: 'mletter';
+		src: url('<?php echo get_theme_file_uri() ?>/dist/fonts/m/font/mletter.eot?16255687');
+		src: url('<?php echo get_theme_file_uri() ?>/dist/fonts/m/font/mletter.eot?16255687#iefix') format('embedded-opentype'),
+			url('<?php echo get_theme_file_uri() ?>/dist/fonts/m/font/mletter.woff2?16255687') format('woff2'),
+			url('<?php echo get_theme_file_uri() ?>/dist/fonts/m/font/mletter.woff?16255687') format('woff'),
+			url('<?php echo get_theme_file_uri() ?>/dist/fonts/m/font/mletter.ttf?16255687') format('truetype'),
+			url('<?php echo get_theme_file_uri() ?>/dist/fonts/m/font/mletter.svg?16255687#mletter') format('svg');
+		font-weight: normal;
+		font-style: normal;
+	}
+
+	/* Chrome hack: SVG is rendered more smooth in Windozze. 100% magic, uncomment if you need it. */
+	/* Note, that will break hinting! In other OS-es font will be not as sharp as it could be */
+	/*
+@media screen and (-webkit-min-device-pixel-ratio:0) {
+  @font-face {
+    font-family: 'mletter';
+    src: url('../font/mletter.svg?16255687#mletter') format('svg');
+  }
+}
+*/
+	[class^="icon-"]:before,
+	[class*=" icon-"]:before {
+		font-family: "mletter";
+		font-style: normal;
+		font-weight: normal;
+		speak: never;
+
+		display: inline-block;
+		text-decoration: inherit;
+		width: .7em;
+		margin-right: .2em;
+		text-align: center;
+		/* opacity: .8; */
+
+		/* For safety - reset parent styles, that can break glyph codes*/
+		font-variant: normal;
+		text-transform: none;
+
+		/* fix buttons height, for twitter bootstrap */
+		line-height: 1em;
+
+		/* Animation center compensation - margins should be symmetric */
+		/* remove if not needed */
+		margin-left: .2em;
+
+		/* you can be more comfortable with increased icons size */
+		/* font-size: 120%; */
+
+		/* Font smoothing. That was taken from TWBS */
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		/* Uncomment for 3D effect */
+		/* text-shadow: 1px 1px 1px rgba(127, 127, 127, 0.3); */
+	}
+
+	.icon-m-vec {
+		position: relative;
+		top: 1.1rem;
+	}
+
+	.icon-m-vec:before {
+		content: '\e800';
+		transform: scale(.75);
+	}
+
+	.demo-icon {
+		font-family: "mletter";
+		font-style: normal;
+		font-weight: normal;
+		speak: never;
+
+		display: inline-block;
+		text-decoration: inherit;
+		width: 1em;
+		margin-right: .2em;
+		text-align: center;
+		/* opacity: .8; */
+
+		/* For safety - reset parent styles, that can break glyph codes*/
+		font-variant: normal;
+		text-transform: none;
+
+		/* fix buttons height, for twitter bootstrap */
+		line-height: 1em;
+
+		/* Animation center compensation - margins should be symmetric */
+		/* remove if not needed */
+		/* margin-left: .2em; */
+
+		/* You can be more comfortable with increased icons size */
+		/* font-size: 120%; */
+
+		/* Font smoothing. That was taken from TWBS */
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		/* Uncomment for 3D effect */
+		/* text-shadow: 1px 1px 1px rgba(127, 127, 127, 0.3); */
+	}
+
+	/* '' */
+	</style>
+
 
 
 	<?php wp_head(); ?>
--- page-tiempos.php
+++ page-tiempos.php
@@ -7,11 +7,21 @@
 <div class="container">
 	<div class="row">
 		<div class="col border">
-			<h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat soluta dolores modi iure non natus!
+			<!-- <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat soluta dolores modi iure non natus!
 				Hic harum natus mollitia amet quia molestias voluptates, laudantium, minima, inventore dicta sequi ipsa
 				ipsum maxime accusantium voluptate?</h1>
 
-			<h1 class="font-tiempos" style="font-size: 130px;">Over Ons</h1>
+			<h1 class="font-tiempos" style="font-size: 130px;">Over Ons</h1> -->
+
+			<h1 style="font-size: 150px;" class="h1 font-tiempos color-green">Een kleine agency <span
+					class="icon-m-vec"></span>et een
+				grote impact
+			</h1>
+
+			<div class="span3" title="Code: 0xe800">
+				<i class="demo-icon icon-m-vec">&#xe800;</i> <span class="i-name">icon-m-vec</span><span
+					class="i-code">0xe800</span>
+			</div>
 		</div>
 	</div>
 </div>
